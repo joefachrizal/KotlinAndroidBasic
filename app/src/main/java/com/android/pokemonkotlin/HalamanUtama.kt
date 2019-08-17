a172f67a52ecf8e766523780f500243bb8b22c35
joefachrizal <joefachrizal@gmail.com>
2019-08-17 15:05:25 +0700

    add Halaman Utama

--- app/src/main/java/com/android/pokemonkotlin/HalamanUtama.kt
+++ app/src/main/java/com/android/pokemonkotlin/HalamanUtama.kt
@@ -1,9 +1,14 @@
 package com.android.pokemonkotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import com.android.pokemonkotlin.latihandua.IntentAct
+import com.android.pokemonkotlin.latihanempat.RecyclerAct
+import com.android.pokemonkotlin.latihansatu.Activity
+import com.android.pokemonkotlin.latihantiga.ViewGroup
 
 class HalamanUtama : AppCompatActivity() {
 
@@ -14,17 +19,25 @@ class HalamanUtama : AppCompatActivity() {
 
     fun onLatihan1(view: View) {
         Toast.makeText(this, "kamu milih akuu", Toast.LENGTH_SHORT).show()
+        val moveWithDataIntent = Intent(this@HalamanUtama, Activity::class.java)
+        startActivity(moveWithDataIntent)
     }
 
     fun onLatihan2(view: View) {
         Toast.makeText(this, "kamu milih akuu", Toast.LENGTH_SHORT).show()
+        val moveWithDataIntent = Intent(this@HalamanUtama, IntentAct::class.java)
+        startActivity(moveWithDataIntent)
     }
 
     fun onLatihan3(view: View) {
         Toast.makeText(this, "kamu milih akuu", Toast.LENGTH_SHORT).show()
+        val moveWithDataIntent = Intent(this@HalamanUtama, ViewGroup::class.java)
+        startActivity(moveWithDataIntent)
     }
 
     fun onLatihan4(view: View) {
         Toast.makeText(this, "kamu milih akuu", Toast.LENGTH_SHORT).show()
+        val moveWithDataIntent = Intent(this@HalamanUtama, RecyclerAct::class.java)
+        startActivity(moveWithDataIntent)
     }
 }
